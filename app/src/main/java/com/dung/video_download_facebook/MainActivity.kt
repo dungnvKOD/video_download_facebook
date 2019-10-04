ad44882d72bf9d523d7066b8bb45a7c01be537c2
Dũng Nguyễn <dungnv92734@gmail.com>
2019-10-04 10:34:07 +0700

    Initial commit

---
+++ app/src/main/java/com/dung/video_download_facebook/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.dung.video_download_facebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
